9dc52dd463cb55cce78169fcb6c54f66524fbf67
Jover <joverlee521@gmail.com>
2019-11-04 10:58:21 -0800

    create shared `map_symptom`
    Refactor REDCap DET ETLs to use shared `map_symptom`

--- lib/seattleflu/id3c/cli/command/etl/redcap_map.py
+++ lib/seattleflu/id3c/cli/command/etl/redcap_map.py
@@ -41,6 +41,39 @@ def map_vaccine(vaccine_response: str) -> Optional[str]:
     return vaccine_map[vaccine_response]
 
 
+def map_symptom(symptom_name: str) -> Optional[str]:
+    """
+    Maps a *symptom_name* to current symptom values in ID3C warehouse.
+
+    There is no official standard for symptoms, we are using the values
+    created by Audere from year 1 (2018-2019).
+    """
+    symptom_map = {
+        'Feeling feverish':                     'feelingFeverish',
+        'Headache':                             'headaches',
+        'Headaches':                             'headaches',
+        'Cough':                                'cough',
+        'Chills or shivering':                  'chillsOrShivering',
+        'Sweats':                               'sweats',
+        'Sore throat or itchy/scratchy throat': 'soreThroat',
+        'Nausea or vomiting':                   'nauseaOrVomiting',
+        'Runny or stuffy nose':                 'runnyOrStuffyNose',
+        'Runny / stuffy nose':                 'runnyOrStuffyNose',
+        'Feeling more tired than usual':        'fatigue',
+        'Muscle or body aches':                 'muscleOrBodyAches',
+        'Diarrhea':                             'diarrhea',
+        'Ear pain or ear discharge':            'earPainOrDischarge',
+        'Rash':                                 'rash',
+        'Increased trouble with breathing':     'increasedTroubleBreathing',
+        'None of the above':                    None
+    }
+
+    if symptom_name not in symptom_map:
+        raise UnknownSymptomNameError(f"Unknown symptom name «{symptom_name}»")
+
+    return symptom_map[symptom_name]
+
+
 class UnknownSexError(ValueError):
     """
     Raised by :function: `map_sex` if a provided *sex_response*
@@ -53,5 +86,13 @@ class UnknownVaccineResponseError(ValueError):
     """
     Raised by :function: `map_vaccine` if a provided
     *vaccine_response* is not among a set of expected values
+    """
+    pass
+
+
+class UnknownSymptomNameError(ValueError):
+    """
+    Raised by :function: `map_symptom` if a provided
+    *symptom_name* is not among a set of expected values
     """
     pass
