8b5725432456da98189ce2e12c3391a41fbc0e2b
Jover <joverlee521@gmail.com>
2019-11-18 16:16:09 -0800

    augur-build-exporter: update grants
    Update to `shipping.metadata_for_augur_build_v2`

--- schema/verify/roles/augur-build-exporter/grants@2019-10-30.sql
+++ schema/verify/roles/augur-build-exporter/grants@2019-10-30.sql
@@ -4,10 +4,10 @@ begin;
 
 select 1/pg_catalog.has_database_privilege('augur-build-exporter', :'DBNAME', 'connect')::int;
 select 1/pg_catalog.has_schema_privilege('augur-build-exporter', 'shipping', 'usage')::int;
-select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v2', 'select')::int;
+select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v1', 'select')::int;
 select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.genomic_sequences_for_augur_build_v1', 'select')::int;
 
-select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v2', 'insert,update,delete'))::int;
+select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v1', 'insert,update,delete'))::int;
 select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.genomic_sequences_for_augur_build_v1', 'insert,update,delete'))::int;
 
 rollback;
