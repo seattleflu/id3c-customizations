69bcc9b0484f04c574361705e15255a9f1bdbf3a
Jover <joverlee521@gmail.com>
2019-11-18 16:39:32 -0800

    api: Add augur-export endpoints to custom routes

--- lib/seattleflu/id3c/api/routes.py
+++ lib/seattleflu/id3c/api/routes.py
@@ -1,5 +1,5 @@
 import logging
-from flask import jsonify
+from flask import jsonify, Response
 from flask_cors import cross_origin
 from id3c.api.routes import api_v1
 from id3c.api.utils.routes import authenticated_datastore_session_required
@@ -17,3 +17,32 @@ def get_barcode_results(barcode, session):
     LOG.debug(f"Exporting presence/absence results for <{barcode}>")
     results = datastore.fetch_barcode_results(session, barcode)
     return jsonify(results)
+
+
+@api_v1.route("/shipping/augur-build-metadata", methods = ['GET'])
+@authenticated_datastore_session_required
+def get_metadata(session):
+    """
+    Export metadata needed for SFS augur build
+    """
+    LOG.debug("Exporting metadata for SFS augur build")
+
+    metadata = datastore.fetch_metadata_for_augur_build(session)
+
+    return Response((row[0] + '\n' for row in metadata), mimetype="application/x-ndjson")
+
+
+@api_v1.route("/shipping/genomic-data/<lineage>/<segment>", methods = ['GET'])
+@authenticated_datastore_session_required
+def get_genomic_data(lineage, segment, session):
+    """
+    Export genomic data needed for SFS augur build based on provided
+    *lineage* and *segment*.
+    The *lineage* should be in the full lineage in ltree format
+    such as 'Influenza.A.H1N1'
+    """
+    LOG.debug(f"Exporting genomic data for lineage <{lineage}> and segment <{segment}>")
+
+    sequences = datastore.fetch_genomic_sequences(session, lineage, segment)
+
+    return Response((row[0] + '\n' for row in sequences), mimetype="application/x-ndjson")
