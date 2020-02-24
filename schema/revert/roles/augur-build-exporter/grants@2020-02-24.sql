ce1d866e2ac46e9789d8d01461f2ee5ef5fe8f37
Jover <joverlee521@gmail.com>
2020-02-24 11:50:18 -0800

    augur-build-exporter: Update grants
    Add `shipping.incidence_model_observation_v4`, `warehouse.puma`,
    and `warehouse.neighborhood_district`.
    
    Remove grants on `warehouse.tract`.

--- schema/revert/roles/augur-build-exporter/grants@2020-02-24.sql
+++ schema/revert/roles/augur-build-exporter/grants@2020-02-24.sql
@@ -13,12 +13,9 @@ grant usage
     to "augur-build-exporter";
 
 grant select
-    on shipping.metadata_for_augur_build_v4,
-       shipping.metadata_for_augur_build_v3,
-       shipping.metadata_for_augur_build_v2,
+    on shipping.metadata_for_augur_build_v2,
        shipping.genomic_sequences_for_augur_build_v1,
-       warehouse.puma,
-       warehouse.neighborhood_district
+       warehouse.tract
     to "augur-build-exporter";
 
 commit;
--- schema/verify/roles/augur-build-exporter/grants@2020-02-24.sql
+++ schema/verify/roles/augur-build-exporter/grants@2020-02-24.sql
@@ -4,18 +4,14 @@ begin;
 
 select 1/pg_catalog.has_database_privilege('augur-build-exporter', :'DBNAME', 'connect')::int;
 select 1/pg_catalog.has_schema_privilege('augur-build-exporter', 'shipping', 'usage')::int;
-select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v4', 'select')::int;
 select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v3', 'select')::int;
 select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v2', 'select')::int;
 select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.genomic_sequences_for_augur_build_v1', 'select')::int;
-select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.puma', 'select')::int;
-select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.neighborhood_district', 'select')::int;
+select 1/pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.tract', 'select')::int;
 
-select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v4', 'insert,update,delete'))::int;
 select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v3', 'insert,update,delete'))::int;
 select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.metadata_for_augur_build_v2', 'insert,update,delete'))::int;
 select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'shipping.genomic_sequences_for_augur_build_v1', 'insert,update,delete'))::int;
-select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.puma', 'insert,update,delete'))::int;
-select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.neighborhood_district', 'insert,update,delete'))::int;
+select 1/(not pg_catalog.has_table_privilege('augur-build-exporter', 'warehouse.tract', 'insert,update,delete'))::int;
 
 rollback;
