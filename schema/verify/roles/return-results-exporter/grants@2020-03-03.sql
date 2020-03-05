42343203c9f7b8d32c480a684f4fb8b8cb407777
Kairsten Fay <kairsten.fay@gmail.com>
2020-03-05 12:46:42 -0800

    Update return-results-exporter grants for v2
    Update the "return-results-exporter" grants to be able to use the new
    view, `shipping.return_results_v2`.

--- schema/verify/roles/return-results-exporter/grants@2020-03-03.sql
+++ schema/verify/roles/return-results-exporter/grants@2020-03-03.sql
@@ -6,9 +6,7 @@ begin;
 select 1/pg_catalog.has_database_privilege('return-results-exporter', :'DBNAME', 'connect')::int;
 select 1/pg_catalog.has_schema_privilege('return-results-exporter', 'shipping', 'usage')::int;
 select 1/pg_catalog.has_table_privilege('return-results-exporter', 'shipping.return_results_v1', 'select')::int;
-select 1/pg_catalog.has_table_privilege('return-results-exporter', 'shipping.return_results_v2', 'select')::int;
 
 select 1/(not pg_catalog.has_table_privilege('return-results-exporter', 'shipping.return_results_v1', 'insert,update,delete'))::int;
-select 1/(not pg_catalog.has_table_privilege('return-results-exporter', 'shipping.return_results_v2', 'insert,update,delete'))::int;
 
 rollback;
