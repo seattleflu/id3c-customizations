f9ddebbeb99fb401fe9ab6962dd4617b14b0f8df
Thomas Sibley <tsibley@fredhutch.org>
2020-03-08 15:26:12 -0700

    Remove anachronistic grant
    A past rework of roles/augur-build-exporter/grants in "roles: Update
    augur-build-exporter for v3" (5ea943f) accidentally modified both the
    …/grants.sql script and the …/grants@2020-01-31.sql script.  It should
    have modified just the former.
    
    This anachronism didn't cause issues at the time because the SQL script
    here was never run, but it errors when deploying from scratch or from a
    much older point.
    
    It's an easy mistake to make, and a better CI setup would have caught
    this for us before merge by
    
        a) ensuring deploy from scratch works, and/or
        b) ensuring reworks copy X.sql → X@TAG.sql without any changes.

--- schema/deploy/roles/augur-build-exporter/grants@2020-01-31.sql
+++ schema/deploy/roles/augur-build-exporter/grants@2020-01-31.sql
@@ -13,8 +13,7 @@ grant usage
     to "augur-build-exporter";
 
 grant select
-    on shipping.metadata_for_augur_build_V3,
-       shipping.metadata_for_augur_build_v2,
+    on shipping.metadata_for_augur_build_v2,
        shipping.genomic_sequences_for_augur_build_v1,
        warehouse.tract
     to "augur-build-exporter";
