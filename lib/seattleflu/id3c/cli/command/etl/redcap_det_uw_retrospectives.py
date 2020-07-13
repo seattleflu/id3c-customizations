d5d331eeb1f675647b7107eb90e8cf7e74e2225e
Jover <joverlee521@gmail.com>
2020-07-13 09:15:01 -0700

    etl redcap-det uw-retro: add new discharge disposition value

--- lib/seattleflu/id3c/cli/command/etl/redcap_det_uw_retrospectives.py
+++ lib/seattleflu/id3c/cli/command/etl/redcap_det_uw_retrospectives.py
@@ -305,6 +305,7 @@ def discharge_disposition(redcap_record: dict) -> Optional[str]:
         'home: home/self care'                                  : 'home',
         'hospice - home'                                        : 'hosp',
         'hospice-hm: hospice - home'                            : 'hosp',
+        'hospice - medical facility'                            : 'hosp',
         'ltc: disch/transferred to long-term care hosp'         : 'long',
         'disch/trans to court/law enforcement'                  : 'oth',
         'disch/trans : disch/trans to court/law enforcement'    : 'oth',
