91a64d065ae2e7fe2533df242cba5bdc19799ed2
Jover <joverlee521@gmail.com>
2020-07-23 16:34:52 -0700

    redcap-det scan: update check for `specimen_received`
    Only check `nasal_swab_collection` is complete if the
    `post_collection_data_entry_qc` instrument does not exist.
    
    This will stop the extra "lack of barcode" warnings from the mail-in
    projects.

--- lib/seattleflu/id3c/cli/command/etl/redcap_det_scan.py
+++ lib/seattleflu/id3c/cli/command/etl/redcap_det_scan.py
@@ -146,13 +146,15 @@ def redcap_det_scan(*, db: DatabaseSession, cache: TTLCache, det: dict, redcap_r
 
     specimen_entry = None
     specimen_observation_entry = None
+    specimen_received = is_complete('post_collection_data_entry_qc', redcap_record)
+
     # Mail in SCAN projects have `post_collection_data_entry_qc` instrument to
     # indicate a specimen is received. The SCAN In-Person Enrollmen project
     # does not have that instrument. So we rely on `nasal_swab_collection`
     # instrument to know that we have sample data to ingest.
     #   -Jover, 16 July 2020
-    specimen_received = is_complete('post_collection_data_entry_qc', redcap_record) or \
-                        is_complete('nasal_swab_collection', redcap_record)
+    if specimen_received is None:
+        specimen_recieved = is_complete('nasal_swab_collection', redcap_record)
 
     if specimen_received:
         specimen_entry, specimen_reference = create_specimen(redcap_record, patient_reference)
