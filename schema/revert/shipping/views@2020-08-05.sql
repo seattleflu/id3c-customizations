c4fe0846ee1ef0eea81d7e5722aa5dea8625423a
Jover <joverlee521@gmail.com>
2020-08-11 17:38:12 -0700

    shipping.scan_enrollments_v1: add `geo_location_name` to match GeoJSON
    Include a human readable location name for neighborhood district
    or PUMA so that it can be linked to the location name in the GeoJSON.
    This link must be done using the human readable name because the linking
    column is what shows up on hover of the PowerBI map.

--- schema/revert/shipping/views@2020-08-05.sql
+++ schema/revert/shipping/views@2020-08-05.sql
@@ -2191,7 +2191,7 @@ grant select
     to "scan-dashboard-exporter";
 
 
-create or replace view shipping.scan_hcov19_result_counts_v1 as
+create or replace view shipping.scan_hcov19_positives_v1 as
 
     with hcov19_presence_absence as (
         -- Collapse potentially multiple hCoV-19 results
@@ -2251,55 +2251,60 @@ create or replace view shipping.scan_hcov19_result_counts_v1 as
     select
         hcov19_result_release_date,
         count(*) filter (where hcov19_result in ('positive', 'inconclusive')) as total_hcov19_positives,
-        count(*) filter (where hcov19_result = 'negative') as total_hcov19_negatives,
         count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'king') as king_county_positives,
-        count(*) filter (where hcov19_result = 'negative' and county = 'king') as king_county_negatives,
         count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'snohomish') as snohomish_county_positives,
-        count(*) filter (where hcov19_result = 'negative' and county = 'snohomish') as snohomish_county_negatives,
         count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'yakima') as yakima_county_positives,
-        count(*) filter (where hcov19_result = 'negative' and county = 'yakima') as yakima_county_negatives,
-        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county is null) as other_positives,
-        count(*) filter (where hcov19_result = 'negative' and county is null) as other_negatives
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county is null) as other_positives
     from scan_hcov19_results
     group by hcov19_result_release_date
 ;
 
-comment on view shipping.scan_hcov19_result_counts_v1 is
-  'A view of counts of hcov19 results from the SCAN project grouped by date results were released.';
+comment on view shipping.scan_hcov19_positives_v1 is
+  'A view of counts of hcov19 positives from the SCAN project grouped by date results were released.';
 
--- Even if it's just aggregate counts of hcov19 results,
+-- Even if it's just aggregate counts of hcov19 positives,
 -- we should probably restrict access to this view to only hcov19-visibility
 -- and scan-dashboard-exporter.
 --  -Jover, 9 July 2020
-revoke all on shipping.scan_hcov19_result_counts_v1 from reporter;
+revoke all on shipping.scan_return_results_v1 from reporter;
 
 grant select
-    on shipping.scan_hcov19_result_counts_v1
+    on shipping.scan_return_results_v1
     to "hcov19-visibility";
 
 revoke all
-    on shipping.scan_hcov19_result_counts_v1
+    on shipping.scan_hcov19_positives_v1
   from "scan-dashboard-exporter";
 
 grant select
-    on shipping.scan_hcov19_result_counts_v1
+    on shipping.scan_hcov19_positives_v1
+    to "scan-dashboard-exporter";
+
+
+create or replace view shipping.seattle_neighborhood_districts_v1 as
+
+    select
+        identifier,
+        st_assvg(point, rel=>1, maxdecimaldigits=>5) as centroid,
+        st_assvg(polygon, rel=>1, maxdecimaldigits=>5) as svg_path
+    from warehouse.location
+    where scale = 'neighborhood_district'
+    and hierarchy -> 'city' = 'seattle'
+;
+
+comment on view shipping.seattle_neighborhood_districts_v1 is
+  'A view of Seattle neighborhood district polygons as SVG paths';
+
+revoke all
+    on shipping.seattle_neighborhood_districts_v1
+  from "scan-dashboard-exporter";
+
+grant select
+    on shipping.seattle_neighborhood_districts_v1
     to "scan-dashboard-exporter";
 
 
 create or replace view shipping.scan_enrollments_v1 as
-
-    with location_names as (
-        select
-            scale,
-            identifier,
-            case scale
-                when 'neighborhood_district' then details->>'name'
-                when 'puma' then details->>'NAMELSAD10'
-            end as geo_location_name
-        from warehouse.location
-        where scale in ('neighborhood_district', 'puma')
-        and hierarchy @> 'state => washington'
-    )
 
     select
         illness_questionnaire_date,
@@ -2307,12 +2312,9 @@ create or replace view shipping.scan_enrollments_v1 as
         priority_code,
         puma,
         neighborhood_district,
-        geo_location_name,
         sample is not null or never_tested is not null as kit_received
 
     from shipping.scan_encounters_v1
-    join location_names on coalesce(neighborhood_district,puma) = lower(location_names.identifier)
-
 ;
 
 comment on view shipping.scan_enrollments_v1 is
