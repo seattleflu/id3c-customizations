d96244925038a454bc4a24dbf44a0c5a20a089fc
Thomas Sibley <tsibley@fredhutch.org>
2020-09-11 14:32:45 -0700

    Add a new schema, operations
    …for data supporting operations management processes.
    
    This will initially be used for implementing a test quota service for
    the Husky Coronavirus Testing study, integrated with its REDCap project.

---
+++ schema/deploy/operations/schema.sql
@@ -0,0 +1,9 @@
+-- Deploy seattleflu/id3c-customizations:operations/schema to pg
+
+begin;
+
+create schema operations;
+
+comment on schema operations is 'Data supporting opertions management processes';
+
+commit;
