de655dae7f9b7b691bd2db8f4c96e0759d77a921
Todd Seidelmann <seidelma@uw.edu>
2021-07-15 10:38:11 -0700

    Add shipping.scan_hcov19_result_counts_v2 view for SCAN dashboard
    In order to exclude kits that have been shipped but not yet tested
    from positivity counts on the SCAN dashboard, it is necessary to
    add the priority code field to the view so we can differentiate
    between those kits that have been confirmed positive and those not
    yet tested.
    
    An API endpoint will also need to be added to use this new view.

--- schema/deploy/shipping/views.sql
+++ schema/deploy/shipping/views.sql
@@ -18,6 +18,7 @@ begin;
 drop view if exists shipping.scan_redcap_enrollments_v1;
 drop view if exists shipping.scan_enrollments_v1;
 drop view if exists shipping.scan_hcov19_result_counts_v1;
+drop view if exists shipping.scan_hcov19_result_counts_v2;
 drop view if exists shipping.scan_demographics_v2;
 drop view if exists shipping.scan_demographics_v1;
 
@@ -2821,6 +2822,73 @@ revoke all
 
 grant select
     on shipping.scan_hcov19_result_counts_v1
+    to "scan-dashboard-exporter";
+
+
+create or replace view shipping.scan_hcov19_result_counts_v2 as
+
+    with scan_hcov19_results as (
+      select
+          case
+              when hcov19_pa.present is true then 'positive'
+              when hcov19_pa.present is false then 'negative'
+              when hcov19_pa.present is null then 'inconclusive'
+          end as hcov19_result,
+          hcov19_result_release_date,
+          case
+              when puma in ('5310901', '5310902') then 'yakima'
+              when puma in ('5311701', '5311702', '5311703',
+                            '5311704', '5311705', '5311706') then 'snohomish'
+              when puma in ('5311601', '5311602', '5311603', '5311604',
+                            '5311605', '5311606', '5311607', '5311608',
+                            '5311609', '5311610', '5311611', '5311612',
+                            '5311613', '5311614', '5311615', '5311616') then 'king'
+              else null
+          end as county,
+          upper(priority_code) as priority_code
+
+      from shipping.scan_encounters_v1
+      join shipping.hcov19_presence_absence_result_v1 as hcov19_pa using (sample_id)
+      where
+        hcov19_pa.sample_id is null or hcov19_pa.details @> '{"assay_type": "Clia"}'
+    )
+
+    select
+        hcov19_result_release_date,
+        priority_code,
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive')) as total_hcov19_positives,
+        count(*) filter (where hcov19_result = 'negative') as total_hcov19_negatives,
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'king') as king_county_positives,
+        count(*) filter (where hcov19_result = 'negative' and county = 'king') as king_county_negatives,
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'snohomish') as snohomish_county_positives,
+        count(*) filter (where hcov19_result = 'negative' and county = 'snohomish') as snohomish_county_negatives,
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county = 'yakima') as yakima_county_positives,
+        count(*) filter (where hcov19_result = 'negative' and county = 'yakima') as yakima_county_negatives,
+        count(*) filter (where hcov19_result in ('positive', 'inconclusive') and county is null) as other_positives,
+        count(*) filter (where hcov19_result = 'negative' and county is null) as other_negatives
+    from scan_hcov19_results
+    group by hcov19_result_release_date, priority_code
+;
+
+comment on view shipping.scan_hcov19_result_counts_v2 is
+  'A view of counts of hcov19 results from the SCAN project grouped by date results were released, with priority codes.';
+
+-- Even if it's just aggregate counts of hcov19 results,
+-- we should probably restrict access to this view to only hcov19-visibility
+-- and scan-dashboard-exporter.
+--  -Jover, 9 July 2020
+revoke all on shipping.scan_hcov19_result_counts_v2 from reporter;
+
+grant select
+    on shipping.scan_hcov19_result_counts_v2
+    to "hcov19-visibility";
+
+revoke all
+    on shipping.scan_hcov19_result_counts_v2
+  from "scan-dashboard-exporter";
+
+grant select
+    on shipping.scan_hcov19_result_counts_v2
     to "scan-dashboard-exporter";
 
 
--- schema/verify/shipping/views@2021-07-13b.sql
+++ schema/verify/shipping/views@2021-07-13b.sql
@@ -137,11 +137,6 @@ select 1/(count(*) = 1)::int
 select 1/(count(*) = 1)::int
   from information_schema.views
  where array[table_schema, table_name]::text[]
-     = pg_catalog.parse_ident('shipping.scan_hcov19_result_counts_v2');
-
-select 1/(count(*) = 1)::int
-  from information_schema.views
- where array[table_schema, table_name]::text[]
      = pg_catalog.parse_ident('shipping.scan_enrollments_v1');
 
 select 1/(count(*) = 1)::int
