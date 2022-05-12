a6d07c9becd2acc0158c0fc7e46aa8ce4aca3c59
David Reinhart <davidrr@uw.edu>
2022-05-12 12:35:55 -0700

    Add API endpoint for deliverables log
    Adding an API endpoint to retrieve records from the deliverables log table, for use
    by LIMS for QC processes.
    
    Since entries in this table only include sample OR collection barcode (depending on the
    process being logged) and LIMS does not have a record of the sample identifiers assigned
    to tiny swabs, this endpoint will return the associated sample and collection barcodes for
    each log entry. The `details` returned for each record are limited to values that may be useful
    for QC.
    
    Access to this endpoint is being limited to localhost and LIMS IP address which are the only
    current use cases.

--- lib/seattleflu/id3c/api/routes.py
+++ lib/seattleflu/id3c/api/routes.py
@@ -1,9 +1,11 @@
 import logging
-from flask import jsonify, Response, Blueprint
+from flask import jsonify, request, abort, Response, Blueprint
 from flask_cors import cross_origin
 from id3c.api.routes import api_v1, blueprints
+from id3c.api.exceptions import BadRequest
 from id3c.api.utils.routes import authenticated_datastore_session_required
 from . import datastore
+import os
 
 LOG = logging.getLogger(__name__)
 
@@ -181,3 +183,26 @@ def get_latest_results(session):
     latest_results = datastore.fetch_rows_from_table(session, ("shipping", "latest_results"))
 
     return Response((row[0] + '\n' for row in latest_results), mimetype="application/x-ndjson")
+
+
+@api_v1.route("/operations/deliverables-log", methods = ['GET'])
+@authenticated_datastore_session_required
+def get_deliverables_log(session):
+    """
+    Export deliverables log
+    """
+    LOG.debug("Exporting deliverables log for LIMS integration")
+
+    # restrict access to LIMS IP and localhost
+    if request.remote_addr not in [os.environ['LIMS_IP'], '127.0.0.1']:
+        abort(403)
+
+    sent_on = request.args.get('sent')
+    process_name = request.args.get('process_name')
+
+    if not sent_on or not process_name:
+        raise BadRequest(f"Missing required argument")
+
+    deliverables_log = datastore.fetch_deliverables_log(session, sent_on, process_name)
+
+    return Response((row[0] + '\n' for row in deliverables_log), mimetype="application/x-ndjson")
